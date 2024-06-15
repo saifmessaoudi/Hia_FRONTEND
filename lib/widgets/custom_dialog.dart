a8917253b87d0d4cda9f45bd2feed5554519fc3a
saifmessaoudi <saif.messaoudi@esprit.tn>
2024-06-15 04:33:19 +0100

    animate dialog alert

--- lib/widgets/custom_dialog.dart
+++ lib/widgets/custom_dialog.dart
@@ -1,8 +1,5 @@
-// lib/widgets/custom_dialog.dart
-
 import 'package:flutter/material.dart';
 import 'package:hia/constant.dart';
-import 'package:nb_utils/nb_utils.dart';
 
 class CustomDialog extends StatelessWidget {
   final String title;
@@ -11,12 +8,12 @@ class CustomDialog extends StatelessWidget {
   final VoidCallback onConfirm;
 
   const CustomDialog({
-    super.key,
+    Key? key,
     required this.title,
     required this.content,
     required this.onCancel,
     required this.onConfirm,
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -26,90 +23,111 @@ class CustomDialog extends StatelessWidget {
       ),
       elevation: 10,
       backgroundColor: Colors.transparent,
-      child: Stack(
-        clipBehavior: Clip.none,
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsets.all(15),
-            decoration: BoxDecoration(
-              color: Colors.transparent,  // fully transparent background
-              borderRadius: BorderRadius.circular(15),
-              border: Border.all(
-                color: kMainColor,
-                width: 3,
-              ),
-              boxShadow: [
-                BoxShadow(
-                  color: black.withOpacity(0.3),
-                  spreadRadius: 2,
-                  blurRadius: 5,
-                  offset: const Offset(0, 2),
-                ),
-              ],
-            ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                const SizedBox(height: 30), // space for the logo
-                Text(
-                  title,
-                  style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,  // make sure text is visible on transparent background
-                  ),
-                ),
-                const SizedBox(height: 10),
-               content != '' ? Text(
-                  content,
-                  style: const TextStyle(
-                    fontSize: 16,
-                    color: Colors.white,  // make sure text is visible on transparent background
-                  ),
-                ) : const SizedBox.shrink(),
-                const SizedBox(height: 15),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    ElevatedButton(
-                      onPressed: onCancel,
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.red,
-                        minimumSize: const Size(100, 40),
-                      ),
-                      child: const Text('Cancel', style: TextStyle(
-                        color: Colors.white,
-                      ),),
-                    ),
-                    const SizedBox(width: 20),
-                    ElevatedButton(
-                      onPressed: onConfirm,
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.green,
-                        minimumSize: const Size(100, 40),
-                      ),
-                      child: const Text('Yes' ,style: TextStyle(
-                        color: Colors.white,
-                      ),),
-                    ),
-                  ],
-                ),
-              ],
-            ),
+      child: AnimatedBuilder(
+        animation: Tween<double>(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+          parent: ModalRoute.of(context)!.animation!,
+          curve: Curves.easeInOut,
+        )),
+        builder: (context, child) => Opacity(
+          opacity: ModalRoute.of(context)!.animation!.value,
+          child: Transform.scale(
+            scale: ModalRoute.of(context)!.animation!.value,
+            child: child,
           ),
-          Positioned(
-            top: -30,
-            left: 0,
-            right: 0,
-            child: Center(
-              child: Image.asset(
-                'images/h_logo.png',
-                width: 60,
-                height: 60,
+        ),
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+                borderRadius: BorderRadius.circular(15),
+                border: Border.all(
+                  color: kMainColor,
+                  width: 3,
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.3),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: const Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const SizedBox(height: 30), // space for the logo
+                  Text(
+                    title,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  content != ''
+                      ? Text(
+                          content,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                          ),
+                        )
+                      : const SizedBox.shrink(),
+                  const SizedBox(height: 15),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      ElevatedButton(
+                        onPressed: onCancel,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.red,
+                          minimumSize: const Size(100, 40),
+                        ),
+                        child: const Text(
+                          'Cancel',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 20),
+                      ElevatedButton(
+                        onPressed: onConfirm,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.green,
+                          minimumSize: const Size(100, 40),
+                        ),
+                        child: const Text(
+                          'Yes',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
-          ),
-        ],
+            Positioned(
+              top: -30,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: Image.asset(
+                  'images/h_logo.png',
+                  width: 60,
+                  height: 60,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
